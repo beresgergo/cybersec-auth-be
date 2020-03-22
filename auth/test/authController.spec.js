c6551758e4a15f9dedf0c7e2e244c5767b5345e8
Gergely Béres <gergely.beres@adnovum.hu>
2020-03-22 16:34:22 +0100

    Sample unit test for auth component

---
+++ auth/test/authController.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+// eslint-disable-next-line no-undef
+describe('Sample', function() {
+    it('true should be always equal to true', function() {
+        expect(true).to.be.equal(true);
+    });
+});
