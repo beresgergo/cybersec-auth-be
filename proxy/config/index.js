4159f09475c3d7a9bf82bbc4416778ff8a2ba7ef
Gergely Béres <gergely.beres@adnovum.hu>
2020-03-23 08:17:18 +0100

    CI adjusted, now after every commit there is also an eslint check executed.

--- proxy/config/index.js
+++ proxy/config/index.js
@@ -3,16 +3,16 @@
 const fs = require('fs');
 
 module.exports.authBaseConfig = {
-	key: fs.readFileSync('/var/opt/certs/cyberauth_proxy.key'),
-	cert: fs.readFileSync('/var/opt/certs/cyberauth_proxy.crt'),
-	ca: fs.readFileSync('/var/opt/certs/cyberauth_ca.crt'),
-	uri: 'https://auth:8000/auth',
-	json: true
+    key: fs.readFileSync('/var/opt/certs/cyberauth_proxy.key'),
+    cert: fs.readFileSync('/var/opt/certs/cyberauth_proxy.crt'),
+    ca: fs.readFileSync('/var/opt/certs/cyberauth_ca.crt'),
+    uri: 'https://auth:8000/auth',
+    json: true
 };
 
 module.exports.getProtectedResourceConfig = {
-	key: fs.readFileSync('/var/opt/certs/cyberauth_proxy.key'),
-	cert: fs.readFileSync('/var/opt/certs/cyberauth_proxy.crt'),
-	ca: fs.readFileSync('/var/opt/certs/cyberauth_ca.crt'),
-	uri: 'https://auth:8000/protected'
+    key: fs.readFileSync('/var/opt/certs/cyberauth_proxy.key'),
+    cert: fs.readFileSync('/var/opt/certs/cyberauth_proxy.crt'),
+    ca: fs.readFileSync('/var/opt/certs/cyberauth_ca.crt'),
+    uri: 'https://auth:8000/protected'
 };
--- proxy/controllers/authController.js
+++ proxy/controllers/authController.js
@@ -6,41 +6,41 @@ const request = require('request-promise-native');
 const HTTP_OK = 200;
 
 module.exports.getAuthenticationToken = (req, res) => {
-	const promise = request.get(base.authBaseConfig);
+    const promise = request.get(base.authBaseConfig);
 
-	promise.then(response => {
-		return res
-			.status(HTTP_OK)
-			.json({
-				authorizationToken: response.token
-			});
-	}, () => {
-		return res
-			.status(401)
-			.json({
-				failed: 'failed'
-			});
-	});
+    promise.then(response => {
+        return res
+            .status(HTTP_OK)
+            .json({
+                authorizationToken: response.token
+            });
+    }, () => {
+        return res
+            .status(401)
+            .json({
+                failed: 'failed'
+            });
+    });
 };
 
 module.exports.getProtectedResource = (req, res) => {
-	const opts = base.getProtectedResourceConfig;
-	opts.json = req.body;
-	const promise = request.post(opts);
+    const opts = base.getProtectedResourceConfig;
+    opts.json = req.body;
+    const promise = request.post(opts);
 
-	promise.then(response => {
-		return res
-			.status(HTTP_OK)
-			.json(response);
-	}, () => {
-		return res.status(401).json({});
-	});
+    promise.then(response => {
+        return res
+            .status(HTTP_OK)
+            .json(response);
+    }, () => {
+        return res.status(401).json({});
+    });
 };
 
 module.exports.ping = (req, res) => {
-	return res
-			.status(HTTP_OK)
-			.json({
-				status: 'OK'
-			});
+    return res
+        .status(HTTP_OK)
+        .json({
+            status: 'OK'
+        });
 };
